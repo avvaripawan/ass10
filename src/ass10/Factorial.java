a82a9197151f92aa4a5f974a0b30aa753debb23b
avvaripavan <avvari.pavan9@gmail.com>
2019-09-25 16:10:03 +0530

    ALL testcase Passed

---
+++ src/ass10/Factorial.java
@@ -0,0 +1,39 @@
+package ass10;
+
+/**
+ *
+ */
+
+public class Factorial {
+
+    /**
+     * This method finds the factorial of a value n. If n is in the range of
+     * 1 to 20 (inclusive) and retuns the factorial otherwise -1
+     *
+     * @param n, the integer n.
+     * @return factorial of n if n is in 1 to 20 (inclusive) and -1 otherwise.
+     */
+    public static long factorial(int n)
+    {
+        if (n <= 20 && 0<n)
+        {
+            long sum = 1;
+            for (int i = n; i > 0; i--) {
+                sum = sum * i;
+            }
+            {
+                return sum;
+            }
+        }
+        else if (n==0)
+        {
+            return 1;
+        }
+        else
+            {
+            return -1;
+        }
+    }
+}
+
+
